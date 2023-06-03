a64c67dc2d2fc917d0ac85d84ce3a2b32d6cca34
Martin <mr.mmaurer@gmail.com>
2023-06-03 23:34:58 +0100

    Removed headless ui from rhf form page

--- example/src/app/validate-form-data/page.tsx
+++ example/src/app/validate-form-data/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
-import { Listbox, Combobox, Switch } from '@headlessui/react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import clsx from 'clsx';
@@ -12,6 +11,8 @@ import BackButton from '@/components/BackButton';
 import { schema } from '../../schema/schema';
 import { titles } from '../../schema/schema.constants';
 import type { Schema, SchemaWithEmptyValues } from '../../schema/schema.types';
+
+//https://stackoverflow.com/questions/73582246/zod-schema-how-to-make-a-field-optional-or-have-a-minimum-string-contraint
 
 const initialFormState: SchemaWithEmptyValues = {
   title: '-',
@@ -68,106 +69,86 @@ export default function ValidateFormData() {
             <legend className="col-span-2 mb-4">Fields</legend>
 
             {/* title */}
-            <Listbox
-              {...register('firstName')}
-              value={getValues('title')}
-              onChange={(value) => {
-                setValue('title', value);
-                trigger('title');
-              }}
-              as="div"
-              className="contents"
+            <label htmlFor="title">
+              Title
+            </label>
+            <select
+              {...register('title')}
+              className='text-black'
+              // onChange={(value) => {
+              //   setValue('title', value);
+              //   trigger('title');
+              // }}
             >
-              <Listbox.Label>Title</Listbox.Label>
-              <div className="relative text-left">
-                <Listbox.Button className="p-2 block w-full text-left bg-white text-black">
-                  {({ value }) => value}
-                </Listbox.Button>
-                <Listbox.Options className="absolute w-full border-t border-black bg-white text-black">
-                  <>
-                    <Listbox.Option key="-" value="-" className="p-2 cursor-pointer">
-                      -
-                    </Listbox.Option>
-                    {titles.map((title) => (
-                      <Listbox.Option key={title} value={title} className="p-2 cursor-pointer">
-                        {title}
-                      </Listbox.Option>
-                    ))}
-                  </>
-                </Listbox.Options>
-                {errors.title && <p className="col-start-2 mt-4">{errors.title.message}</p>}
-              </div>
-            </Listbox>
+              {/* default value */}
+              <option key="-" value="-">-</option>
+              {titles.map((title) => (
+                <option key={title} value={title}>
+                  {title}
+                </option>
+              ))}
+            </select>
+            {errors.title && <p className="col-start-2 mt-4">{errors.title.message}</p>}
 
             {/* firstName */}
-            <Combobox value={() => getValues('firstName')}>
-              <Combobox.Label>First name</Combobox.Label>
-              <Combobox.Input
-                {...register('firstName')}
-                displayValue={() => getValues('firstName')}
-                className="text-black"
-              />
-              {errors.firstName && <p className="col-start-2">{errors.firstName.message}</p>}
-            </Combobox>
+            <label htmlFor="firstName">First name</label>
+            <input
+              {...register('firstName')}
+              className='text-black'
+              id="firstName"
+              type="text"
+            />
+            {errors.firstName && <p className="col-start-2">{errors.firstName.message}</p>}
 
             {/* lastName */}
-            <Combobox value={() => getValues('lastName')}>
-              <Combobox.Label>Last name</Combobox.Label>
-              <Combobox.Input
-                {...register('lastName')}
-                displayValue={() => getValues('lastName')}
-                className="text-black"
-              />
-              {errors.lastName && <p className="col-start-2">{errors.lastName.message}</p>}
-            </Combobox>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              {...register('lastName')}
+              className='text-black'
+              id="lastName"
+              type="text"
+            />
+            {errors.lastName && <p className="col-start-2">{errors.lastName.message}</p>}
 
             {/* middleName */}
-            <Combobox value={() => getValues('middleName')} nullable>
-              <Combobox.Label>Middle name</Combobox.Label>
-              <Combobox.Input
-                {...register('middleName')}
-                displayValue={() => getValues('middleName') ?? ''}
-                className="text-black"
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  const inputValue = event.target.value;
-                  const newValue = inputValue !== '' ? inputValue : undefined;
-                  setValue('middleName', newValue);
-                }}
-              />
-              {errors.middleName && <p className="col-start-2">{errors.middleName.message}</p>}
-            </Combobox>
+            <label htmlFor="middleName">Last name</label>
+            <input
+              {...register('middleName')}
+              className='text-black'
+              id="middleName"
+              type="text"
+              // onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              //   const inputValue = event.target.value;
+              //   const newValue = inputValue !== '' ? inputValue : undefined;
+              //   setValue('middleName', newValue);
+              // }}
+            />
+            {errors.middleName && <p className="col-start-2">{errors.middleName.message}</p>}
 
             {/* has cats */}
-            <Switch.Group>
-              <Switch.Label>Has cats</Switch.Label>
-              <Switch
-                {...register('hasCats')}
-                checked={hasCats}
-                onChange={(value) => setValue('hasCats', value)}
-                className={clsx('w-4 h-4 flex items-center border', {
-                  'bg-black border-white': hasCats,
-                  'bg-white border-black ': !hasCats,
-                })}
-              >
-                <span className="ml-8">{hasCats ? 'Yes' : 'No'}</span>
-              </Switch>
-            </Switch.Group>
+            <label htmlFor="hasCats">Has cats</label>
+            <input
+              {...register('hasCats')}
+              className='text-black'
+              id="hasCats"
+              type="checkbox"
+            />
+            {errors.hasCats && <p className="col-start-2">{errors.hasCats.message}</p>}
 
             {/* numberOfCats */}
-            <Combobox value={() => getValues('numberOfCats')}>
-              <Combobox.Label>Number of cats</Combobox.Label>
-              <Combobox.Input
-                {...register('numberOfCats')}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  const inputValue = event.target.value;
-                  const newValue = inputValue !== '' ? parseInt(inputValue, 10) : undefined;
-                  setValue('numberOfCats', newValue);
-                }}
-                displayValue={() => String(getValues('numberOfCats')) ?? ''}
-                className="text-black"
-              />
-              {errors.numberOfCats && <p className="col-start-2">{errors.numberOfCats.message}</p>}
-            </Combobox>
+            <label htmlFor="numberOfCats">Last name</label>
+            <input
+              {...register('numberOfCats')}
+              className='text-black'
+              id="numberOfCats"
+              type="text"
+              // onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              //   const inputValue = event.target.value;
+              //   const newValue = inputValue !== '' ? parseInt(inputValue, 10) : undefined;
+              //   setValue('numberOfCats', newValue);
+              // }}
+            />
+            {errors.numberOfCats && <p className="col-start-2">{errors.numberOfCats.message}</p>}
           </fieldset>
 
           {isDirty && !isValid && <p className="mb-4">Form contains errors.</p>}
@@ -181,10 +162,6 @@ export default function ValidateFormData() {
             </button>
           </div>
         </form>
-
-        <pre>
-          <code>{JSON.stringify(watch(), undefined, 4)}</code>
-        </pre>
 
         <BackButton cssClass="mt-4" />
       </div>
