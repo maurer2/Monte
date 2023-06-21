36c1af6f433073a0f06cfa5e198872367c659630
Martin <mr.mmaurer@gmail.com>
2023-06-22 00:30:44 +0100

    Added validation errors as payload to validation on the server

--- example/src/app/api/set-data/route.ts
+++ example/src/app/api/set-data/route.ts
@@ -1,5 +1,7 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import { ZodError } from 'zod';
+import { fromZodError } from 'zod-validation-error';
 
 import { schema } from '../../../schema/schema';
 
@@ -10,15 +12,29 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     schema.parse(payload);
     return NextResponse.json(
       {
-        message: 'OK',
+        message: 'Schema valid',
       },
       {
         status: 200,
       },
     );
   } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          status: 'error',
+          data: JSON.stringify(fromZodError(error).details),
+          message: 'Validation error'
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+
     return NextResponse.json(
       {
+        status: 'error',
         message: error instanceof Error ? error.message : 'Error',
       },
       {
--- example/src/app/validate-incoming-data-on-the-server/page.tsx
+++ example/src/app/validate-incoming-data-on-the-server/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 
@@ -8,7 +8,21 @@ import Image from 'next/image';
 import BackButton from '@/components/BackButton';
 
 // import { schema } from '../../schema/schema';
-// import type { Schema } from '../../schema/schema.types';
+import type { Schema } from '../../schema/schema.types';
+import { daysOfWorkWeek } from '../../schema/schema.constants';
+
+type SuccessPayload = {
+  status: 'success',
+  message: string
+};
+
+type ErrorPayload = {
+  status: 'error',
+  data?: string,
+  message: string
+};
+
+type Payload = SuccessPayload | ErrorPayload;
 
 const setApiData = async <T,>(url: string, data: T) => {
   const response = await fetch(url, {
@@ -19,39 +33,78 @@ const setApiData = async <T,>(url: string, data: T) => {
     body: JSON.stringify(data),
   });
 
+  const payload: Payload = await response.json() as Payload;
+
   if (!response.ok) {
-    throw new Error(response.statusText);
+    if (payload.status === 'error') {
+      throw new Error(payload?.data ?? payload?.message);
+    }
   }
 
-  return response;
+  return payload;
+};
+
+const dataValid: Schema = {
+  title: 'Mr.',
+  firstName: 'Cat',
+  lastName: 'Stevens',
+  hasCats: true,
+  numberOfCats: 100,
+  daysInTheOffice: [...daysOfWorkWeek],
+};
+
+const dataInvalid = {
+  title: 'Dude',
+  firstName: 'Cat',
+  lastName: 'Stevens',
+  hasCats: true,
+  numberOfCats: 100,
+  daysInTheOffice: [],
 };
 
 export default function ValidateIncomingDataServer() {
+  const [validDataResponse, setValidDataResponse] = useState<any>(null);
+  const [invalidDataResponse, setInvalidDataResponse] = useState<any>(null);
+
   useEffect(() => {
     (async () => {
       try {
-        // invalid
-        const response = await setApiData('/api/set-valid-data', {
-          title: 'Dude',
-          firstName: 'Cat',
-          lastName: 'Stevens',
-          hasCats: true,
-          numberOfCats: 100,
-          daysInTheOffice: [],
-        });
+        const response = await setApiData('/api/set-data', dataInvalid);
+        setInvalidDataResponse(response);
         console.log(response);
       } catch (error) {
-        console.log(error);
+        if (error instanceof Error) {
+          if (typeof error.message === 'string') {
+            setInvalidDataResponse(error.message);
+          }
+        }
+        // console.log(error instanceof Error ? error.message : 'Error');
       }
     })();
   }, []);
 
   return (
     <main className="min-h-screen p-24 font-sans">
-      <div className="w-full max-w-5xl">
-        <h1 className="mb-4">Validate incoming data on the server</h1>
+      <div className="grid grid-cols-2  w-full max-w-5xl">
+        <h1 className="mb-4 col-span-full">Validate incoming data on the server</h1>
 
-        <Image src="/under-construction.gif" alt="" className="" width={133} height={133} priority />
+        <section>
+          <Image src="/under-construction.gif" alt="" className="" width={133} height={133} priority />
+
+        </section>
+
+        <section>
+          <h2 className="mb-4">Payload 2</h2>
+          <code className="mb-4 block font-mono">
+            <pre className="whitespace-pre-wrap">{JSON.stringify(dataInvalid, undefined, 2)}</pre>
+          </code>
+          <h2 className="mb-4">Response 2</h2>
+          <code className="mb-4 block font-mono">
+            <pre className="whitespace-pre-wrap">
+              {JSON.stringify(JSON.parse(invalidDataResponse), undefined, 2)}
+            </pre>
+          </code>
+        </section>
       </div>
       <BackButton cssClass="mt-4" />
     </main>
